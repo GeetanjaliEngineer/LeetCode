bbbbaca54ad4316ed8dd1b924141afb4d4d069db
geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-10-12 11:05:39 +0530

    Time: 45 ms (29.84%) | Memory: 45.2 MB (32.43%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,12 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
+        //Logic 
+        //input - 2,7,11,15 target is 9 logic is if index[0] + index[1] == ? [9] 
+        //but we use for loop to traverse each array elements so it will go to next element mens 
+        // index++
+//lets take i and j as elements 
+        for(int i=0; i<nums.length;i++ ){
+            //j means i++ = i+1 next value if ith index
             for(int j= i+1; j<nums.length; j++){
-                if(nums[i] +nums[j] ==target){
+                // now condition
+                if(nums[i] + nums[j] == target) {
                     return new int[]{i,j};
                 }
-
             }
-        } return new int[]{};       
+        }
+        return new int[]{};
     }
 }
