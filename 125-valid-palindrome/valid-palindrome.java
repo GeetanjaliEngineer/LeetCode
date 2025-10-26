170961dcf365e48adb33a6f9a6c766ad84ef4e7e
geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-10-26 15:31:33 +0530

    Time: 16 ms (12.86%) | Memory: 45.2 MB (21.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,13 +1,19 @@
 class Solution {
+ 
     public boolean isPalindrome(String s) {
-    s = s.toLowerCase();
-        s= s.replaceAll("[^a-z0-9]","");
-      int  n= s.length();
-        for(int i=0; i<n/2; i++){
-            if(s.charAt(i) != s.charAt(n-1-i)){
-            return false;
-        }
+        // 1 Convert the string to lowercase
+        s = s.toLowerCase();
+
+        // 2 Keep only alphanumeric characters (a-z, 0-9)
+        s = s.replaceAll("[^a-z0-9]", "");
+
+        // 3 Two-pointer check
+        int n = s.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                return false;
+            }
         }
         return true;
-     }
+    }
 }
