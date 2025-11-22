00e253cc9d81135a8d24fa41e464bd17d4a50598
Geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-11-22 18:09:40 +0530

    Time: 2 ms (99.16%) | Memory: 46.9 MB (13.87%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,18 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //Logic 
-        //input - 2,7,11,15 target is 9 logic is if index[0] + index[1] == ? [9] 
-        //but we use for loop to traverse each array elements so it will go to next element mens 
-        // index++
-//lets take i and j as elements 
-        for(int i=0; i<nums.length;i++ ){
-            //j means i++ = i+1 next value if ith index
-            for(int j= i+1; j<nums.length; j++){
-                // now condition
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i,j};
-                }
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+        int complement = target - nums[i];
+        if(map.containsKey(complement)){
+        return new int[]{
+            map.get(complement), i};
+        }
+        map.put(nums[i], i);
         }
         return new int[]{};
     }
