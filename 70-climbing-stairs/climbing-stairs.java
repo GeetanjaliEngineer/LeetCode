06d2c667a56c0a53e96d24a7ba40fdc433eb1bb4
Geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-12-27 22:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (11.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,10 +3,12 @@ class Solution {
         //base case 
         if (n <= 2)
             return n;
+            // f(n)=f(n-1)+f(n-2)
         int first = 1;
         int second = 2;
         for (int i = 3; i <= n; i++) {
             int third = first + second;
+            //swapping
             first = second;
             second = third;
 
