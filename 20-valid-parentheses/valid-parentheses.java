626cb7c392ac122d8757059069ff997e12b5a4cd
Geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2026-01-17 23:31:31 +0530

    Time: 2 ms (97.18%) | Memory: 43.6 MB (12.88%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
+                if (stack.isEmpty()) return false;
+
                 char top = stack.pop();
+
                 if ((c == ')' && top != '(') ||
                     (c == '}' && top != '{') ||
                     (c == ']' && top != '[')) {
@@ -16,6 +17,7 @@ class Solution {
                 }
             }
         }
+
         return stack.isEmpty();
     }
 }
