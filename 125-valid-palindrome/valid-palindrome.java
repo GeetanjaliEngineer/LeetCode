03c19782bb6c7981ea7d053566db9a5a0436810a
Geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2026-04-05 16:32:21 +0530

    Time: 2 ms (99.42%) | Memory: 44.2 MB (79.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,26 +4,20 @@ public class Solution {
         int right = s.length() - 1;
 
         while (left < right) {
-
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
-
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
-
             char cLeft = Character.toLowerCase(s.charAt(left));
             char cRight = Character.toLowerCase(s.charAt(right));
-
             if (cLeft != cRight) {
                 return false;
             }
-
             left++;
             right--;
         }
-
         return true;
     }
 }
