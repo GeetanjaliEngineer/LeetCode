c5e693e1769cad8f60a9dcfa30512df95e6020ec
Geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2026-04-11 18:39:44 +0530

    Time: 1 ms (99.98%) | Memory: 77.3 MB (55.31%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,10 +3,12 @@ class Solution {
         int maxSum = nums[0];
         int currSum = nums[0];
 
-        for(int i = 1; i < nums.length; i++){
+        for( int i =1; i<nums.length; i++){
             currSum = Math.max(nums[i], currSum + nums[i]);
             maxSum = Math.max(maxSum, currSum);
         }
         return maxSum;
-    }
+        }
+        
 }
+
